1c340620ca15580657fa7cc348759118ef82a02c
HiormasaKawakita <kawakita.hiromasa.hdsf@gmail.com>
2018-03-13 01:44:55 +0900

    test file "hello world".

---
+++ hello_world/target/debug/deps/hello_world-620718d586b1439f.d
@@ -0,0 +1,5 @@
+/Users/kawakitahiromasa/test_rust/hello_world/target/debug/deps/hello_world-620718d586b1439f: src/main.rs
+
+/Users/kawakitahiromasa/test_rust/hello_world/target/debug/deps/hello_world-620718d586b1439f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/kawakitahiromasa/test_rust/hello_world/target/debug/hello_world.dSYM: /Users/kawakitahiromasa/test_rust/hello_world/src/main.rs
